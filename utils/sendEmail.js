a4400b1edadc515d492d7fd07f6b3afb6f3ee889
Sherazsabir23 <sherazsabir.dev@gmail.com>
2026-07-11 21:36:27 -0700

    email verifcation code added

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 async function sendEmail(to, subject, html) {
   const { data, error } = await resend.emails.send({
-    from: "onboarding@resend.dev", // Testing ke liye
+   from: "Zentro <noreply@sherazsabir.site>", // Testing ke liye
     to,
     subject,
     html,
